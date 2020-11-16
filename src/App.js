f006c8db25982394d7d7a7b079e909d05f035348
mikedilulo <mikedilulo2019@gmail.com>
2020-11-15 18:30:55 -0700

    Button and form Add Todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+
+  const addTodo = () => {
+    console.log("button clicked");
+  }
+
   return (
     <div className="App">
       <h1>App with Firebase and React.js</h1>
+      <form>
+        <input type="text" />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
     </div>
   );
 }
